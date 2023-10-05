e1fea590e0f3582b06deda5b4078fab1764eba39
LouisBrizuela <louisbrizz@gmail.com>
2023-10-05 13:23:08 -0700

    finished new

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get '/blog_pages' => 'blog_page#index'
-  get '/blog_pages/:id' => 'blog_page#show'
+  get '/blog_pages' => 'blog_page#index', as: "blog_pages"
+  get '/blog_pages/new' => 'blog_page#new', as: "new_blog_page"
+  get '/blog_page/:id' => 'blog_page#show'
 end
