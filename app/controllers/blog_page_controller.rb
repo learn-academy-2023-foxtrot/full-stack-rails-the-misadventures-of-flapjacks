ea0f878e946c28c044118f770a342d4338315ee1
Adrian Ramirez <adrian.ramirez45679@gmail.com>
2023-10-05 16:24:31 -0700

    stories 4-7 completed

--- app/controllers/blog_page_controller.rb
+++ app/controllers/blog_page_controller.rb
@@ -8,4 +8,36 @@ class BlogPageController < ApplicationController
     def new
         @blog_page = BlogPage.new
     end
+
+    def create
+        @blog_page = BlogPage.create(blog_page_params)
+        
+        if @blog_page.valid?
+          redirect_to blog_page_path(@blog_page)#, notice: 'Blog page was successfully created.'
+        #else
+                
+         # render 'new'
+        end
+      end
+      
+      def edit
+        @blog_page = BlogPage.find(params[:id])
+      end
+     
+      def update
+        @blog_page = BlogPage.find(params[:id])
+    
+        if @blog_page.update(blog_page_params)
+          redirect_to blog_page_path(@blog_page), notice: 'Blog page was successfully updated.'
+        else
+         render 'edit'
+        end
+      end
+    
+      private
+      
+      def blog_page_params
+        params.require(:blog_page).permit(:title, :content)
+      end
+      
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get '/blog_pages' => 'blog_page#index', as: "blog_pages"
-  get '/blog_pages/new' => 'blog_page#new', as: "new_blog_page"
-  get '/blog_page/:id' => 'blog_page#show'
+  get '/blog_pages' => 'blog_page#index', as: 'blog_pages'
+  post '/blog_pages' => 'blog_page#create', as: 'create_blog_page'
+  get '/blog_pages/new' => 'blog_page#new', as: 'new_blog_page'
+  get '/blog_pages/:id' => 'blog_page#show', as: 'blog_page'
+  get '/blog_pages/:id/edit' => 'blog_page#edit', as: 'edit_blog_page'
+  patch '/blog_pages/:id' => 'blog_page#update'
 end
